badb489cbfe731ac4f9c1efa480474a8623c11d1
willy <pussinbootsdev@gmail.com>
2021-01-29 16:18:45 +0100

    testing ci

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React maybe
+          Learn React maybe? maybe not
         </a>
       </header>
     </div>
