eadcabd0de686528f6c3cc4c1de9e749bee3fb34
willy <pussinbootsdev@gmail.com>
2021-01-31 16:18:29 +0100

    skeleton 2

--- src/App.js
+++ src/App.js
@@ -10,14 +10,20 @@ function App() {
           <div className="container-fluid">
             <div className="row justify-content-center ">
               <div className="col-9 col-sm-6 col-md-5 col-lg-4 col-xl-3">
-                <img
-                  className="img-fluid ppc-logo"
-                  src="assets/img/peercoin.svg"
-                  alt="peercoin logo"
-                />
+                <a
+                  href="https://ppc.lol/dotnet"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    className="img-fluid ppc-logo"
+                    src="assets/img/peercoin.svg"
+                    alt="peercoin logo"
+                  />
+                </a>
               </div>
             </div>
-            <div className="row justify-content-center mt-5">
+            <div className="row justify-content-center mt-3 mb-3 mt-md-5 mb-md-5 pb-3">
               <span>
                 <img
                   className="ppc-tombstone"
@@ -26,40 +32,59 @@ function App() {
                 ></img>
               </span>
             </div>
-          </div>
-          <hr />
-          <div className="row mt-4">
-            <div className="col">
-              <h3>
-                <strong>Declared dead</strong>
-              </h3>
+            <div className="row justify-content-center">
+              <div className="col-auto">
+                <h4>
+                  <span className="mr-2">
+                    <img src="assets/img/reaper.png" alt="reaper icon" />
+                  </span>
+                  <strong>Declared dead</strong>
+                </h4>
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <h2>5 times</h2>
-          </div>
-          <div className="row mt-5">
-            <div className="col">
-              <h3>
-                <strong>Undead since</strong>{" "}
-              </h3>
+            <div className="row pt-3 pb-3 justify-content-center">
+              <div className="col-auto">
+                <h2 className="m0">5 times</h2>
+              </div>
             </div>
-          </div>
-          <div className="row">
-            <div className="col">
-              <h2>7th of July 2012</h2>
+            <div className="row mt-5 justify-content-center">
+              <div className="col-auto">
+                <h4>
+                  <span className="mr-2">
+                    <img src="assets/img/frankenstein.png" alt="zombie icon" />
+                  </span>
+                  <strong>Undead since</strong>
+                </h4>
+              </div>
+            </div>
+            <div className="row pt- justify-content-center">
+              <div className="col-auto">
+                <h2 className="m0">7th of July 2012</h2>
+              </div>
+            </div>
+            <div className="ppc-arrow_down d-none d-lg-block d-xl-block">
+              <a href="#timeline">
+                <img src="assets/img/arrowDown.png" alt="arrow down" />
+              </a>
             </div>
           </div>
         </div>
       </div>
-      <div className="row">how to contribute goes here</div>
       <div className="row">
-        <div className="col d-flex justify-content-center">
+        <div className="col d-flex justify-content-center" id="timeline">
           <TimeLine />
         </div>
       </div>
-      <div className="row ppc-background justify-content-center p-2">
-        The End - Icons: icons8.de
+      <div className="row ppc-background p-2">
+        <div className="col-6 justify-content-center d-flex">The End </div>
+        <div className="col-6 justify-content-center d-flex">
+          <span>
+            Icons:{" "}
+            <a href="https://ppc.lol/XGWWE" target="_blank" rel="noreferrer">
+              icons8.de
+            </a>
+          </span>
+        </div>
       </div>
     </div>
   );
