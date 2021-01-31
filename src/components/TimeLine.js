138322a0905e0a232c5f50ff3a0310e9311018d9
willy <pussinbootsdev@gmail.com>
2021-01-31 22:01:01 +0100

    fix sorting new to old

--- src/components/TimeLine.js
+++ src/components/TimeLine.js
@@ -60,7 +60,7 @@ const buildTimeLineElement = (
 
 export default function TimeLine({ orbituaries }) {
   function custom_sort(a, b) {
-    return new Date(a.date).getTime() - new Date(b.date).getTime();
+    return new Date(b.date).getTime() - new Date(a.date).getTime();
   }
   orbituaries.sort(custom_sort);
 
